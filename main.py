af0306bf1c2b6f442da98d4d4a9bfea91e2041d8
stas- <stdviaustin@gmail.com>
2021-04-29 16:05:55 +0300

    Added new func "error_returning"

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import requests
 """Ссылка на расписание"""
 BASE_URL = "https://miet.ru/schedule"
 
+def error_returning():
+    return "Error, code isn't 200"
 
 def get_groups():
     """Метод получения списка групп"""
@@ -12,7 +14,7 @@ def get_groups():
     if group_page.status_code == 200:
         return json.loads(group_page.text)
     else:
-        return "get_groups: error, code not 200"
+        error_returning()
 
 
 def get_schedule(group_name: str):
@@ -20,7 +22,7 @@ def get_schedule(group_name: str):
     if schedule_page.status_code == 200:
         return json.loads(schedule_page.text)
     else:
-        return "get_schedule: error, code not 200"
+        error_returning()
 
 
 if __name__ == '__main__':
@@ -30,6 +32,7 @@ if __name__ == '__main__':
 
     for group in group_list[:3]:
         schedule[group] = get_schedule(group)
+
 
     with open("schedule.json", "w+", encoding='utf8') as f:
         f.write(json.dumps(schedule, ensure_ascii=False))
