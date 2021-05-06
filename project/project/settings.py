0765e05bd0b5e700c9ddf4ae987f5edd763e1521
Сидоров Владимир Александрович <Sidorov.V.Ale@sberbank.ru>
2021-05-06 12:34:16 +0300

    add fixtures

--- project/project/settings.py
+++ project/project/settings.py
@@ -107,15 +107,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-Ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
