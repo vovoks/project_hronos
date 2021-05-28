50410288c7da168d804f140d893bbbebb1c725cb
Vladimir Sidorov <sidorov.v.ale@sberbank.ru>
2021-05-28 23:12:06 +0300

    fix

--- project/project/settings.py
+++ project/project/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ekbmcg#k!r-ytld5)19fh1-*me9_!@flso5lgaly3o%^-m(6s)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -163,3 +162,5 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
